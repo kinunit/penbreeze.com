f7aabbb9b14fb6352c127f02ec985b55e9b8f897
Keenlabi <keenlabinc@gmail.com>
2024-09-21 17:36:01 +0100

    add /blog url

--- src/routes/appRoutes.ts
+++ src/routes/appRoutes.ts
@@ -1,4 +1,5 @@
 import About from "src/presentation/pages/About/About.vue";
+import Blog from "src/presentation/pages/Blog/Blog.vue";
 import ContactUs from "src/presentation/pages/ContactUs/ContactUs.vue";
 import LandingPage from "src/presentation/pages/LandingPage";
 import Services from "src/presentation/pages/Services";
@@ -20,6 +21,10 @@ const appRoutes:RouteRecordRaw[] = [
     {
         path:"/contact-us",
         component: ContactUs
+    },
+    {
+        path:"/blog",
+        component: Blog
     }
 ]
 
