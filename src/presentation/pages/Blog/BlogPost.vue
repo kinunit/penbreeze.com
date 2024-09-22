5645a6b6cc91b83731e1ae6f4ced97a83eed7b50
Keenlabi <keenlabinc@gmail.com>
2024-09-22 18:02:48 +0100

    created blog-post page with a child component for post details; post-details abstracts away the async logic that fetches post details to allow the use of a suspense in wait

---
+++ src/presentation/pages/Blog/BlogPost.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import FAQ from 'src/presentation/components/FAQ/FAQ.vue';
+import Footer from 'src/presentation/components/Footer/Footer.vue';
+import { useRoute } from 'vue-router';
+import PostDetails from './components/PostDetails.vue';
+
+const route = useRoute();
+</script>
+<script lang="ts">
+    export default {
+        name:"BlogPost"
+    }
+</script>
+<template>
+    <section class="">
+        <Suspense>
+            <PostDetails :id="route.params.catchAll.toString()" />
+        </Suspense>
+    </section>
+    <FAQ bg2 />
+    <Footer />
+</template>
+<style scoped>
+</style>
---
+++ src/presentation/pages/Blog/components/PostDetails.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { postService } from './posts';
+
+const props = defineProps<{id:string}>()
+
+const postDetails = await postService.getPost(props.id)
+console.log(postDetails)
+</script>
+<script lang="ts">
+    export default {
+        name:"Post"
+    }
+</script>
+<template>
+    <section class="">
+        <div class="title"></div>
+    </section>
+</template>
+<style scoped>
+    .title  {
+
+    }
+</style>
