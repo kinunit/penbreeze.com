1d237a70f989a11437b24fd19a994d9e8d58bd1c
Keenlabi <keenlabinc@gmail.com>
2024-09-22 17:50:30 +0100

    created new catch all route to find a blog post first before rendering page not found; added the name property to all routes

--- src/routes/appRoutes.ts
+++ src/routes/appRoutes.ts
@@ -1,5 +1,6 @@
 import About from "src/presentation/pages/About/About.vue";
 import Blog from "src/presentation/pages/Blog/Blog.vue";
+import BlogPost from "src/presentation/pages/Blog/BlogPost.vue";
 import ContactUs from "src/presentation/pages/ContactUs/ContactUs.vue";
 import LandingPage from "src/presentation/pages/LandingPage";
 import Services from "src/presentation/pages/Services";
@@ -8,27 +9,32 @@ import { type RouteRecordRaw } from "vue-router";
 const appRoutes:RouteRecordRaw[] = [
     {
         path:"/",
+        name:"Landing page",
         component: LandingPage
     },
     {
         path:"/services",
+        name: "Services",
         component: Services
     },
     {
         path:"/about",
+        name:"About",
         component: About
     },
     {
         path:"/contact-us",
+        name:"Contact Us",
         component: ContactUs
     },
     {
         path:"/blog",
+        name:"Blog",
         component: Blog
     },
     {
-        path:"/*",
-        component: 
+        path: "/:catchAll(.*)",
+        component: BlogPost
     }
 ]
 
