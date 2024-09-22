84edfca49b12becd2cd93dd4fe7cde92b2a09579
Keenlabi <keenlabinc@gmail.com>
2024-09-22 17:57:02 +0100

    created a new get post details use case

--- src/application/post/use-cases/index.ts
+++ src/application/post/use-cases/index.ts
@@ -1,17 +1,20 @@
 import type { PostRepository } from "src/domain/post/post.repository";
 import { GetPostsUseCase } from "./GetPostsUseCase";
+import { GetPostDetailsUseCase } from "./GetPostDetailsUseCase";
 
 export class PostService {
     private getPostsUseCase: GetPostsUseCase;
+    private getPostDetailsUseCase: GetPostDetailsUseCase;
 
     constructor(postRepository:PostRepository) {
         this.getPostsUseCase = new GetPostsUseCase(postRepository)
+        this.getPostDetailsUseCase = new GetPostDetailsUseCase(postRepository)
     }
 
     async getPosts() {
         return await this.getPostsUseCase.execute();
     }
     async getPost(postId:string) {
-        // return await this.getPostsUseCase.execute();
+        return await this.getPostDetailsUseCase.execute(postId);
     }
 }
